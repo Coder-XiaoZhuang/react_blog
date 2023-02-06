f6f7d3e6d2397f08022a8e2c8e81dcde59f32bc6
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2023-02-06 17:59:37 +0800

    feat：新增文章列表

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import Head from 'next/head';
-import {Button} from 'antd';
+import {Row, Col, List} from 'antd'
 import Header from '../components/Header';
+import { 
+  CalendarOutlined,
+  FolderOpenOutlined,
+  FireOutlined
+} from '@ant-design/icons';
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Home</title>
-    </Head>
-    <Header />
-    <div><Button>我是按钮</Button></div>
- </>
-)
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Header />
+      <Row className="comm-main" type="flex" justify="center">
+        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
+          左侧
+        </Col>
+  
+        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+          右侧
+        </Col>
+      </Row>
+    </>
+  );
+}
 
-export default Home
+export default Home;
