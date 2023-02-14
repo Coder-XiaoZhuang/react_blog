df85a03be7f0914a6fcd39eb65d1c6ae4f90c6cd
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2023-02-14 17:57:48 +0800

    update：更新侧边栏样式

--- blog/components/Author.js
+++ blog/components/Author.js
@@ -1,12 +1,19 @@
-import { Avatar, Divider } from 'antd';
+import { useState } from 'react';
+import { Avatar, Divider, Tooltip } from 'antd';
 import { 
     GithubOutlined,
     QqOutlined,
     WechatOutlined
   } from '@ant-design/icons';
 import authorStyles from '../static/style/components/Author.module.css';
+import qqUrl from "../images/qq.png";
+import wechatUrl from "../images/wechat.jpg";
 
 const Author = () => {
+    let [state, setState] = useState({
+        isShowQq: false,
+        isShowWechat: false
+    });
 
     return (
         <div className={`comm-box ${authorStyles.authorDiv}`}>
@@ -17,9 +24,33 @@ const Author = () => {
                     <div className={ authorStyles.authorSign }>勇敢，迎难而上。</div>
                 </div>
                 <Divider style={{ color: 'pink'  }}>社交账号</Divider>
-                <Avatar size={28} icon={ <GithubOutlined /> } className={ authorStyles.account }  />
-                <Avatar size={28} icon={ <QqOutlined /> }  className={ authorStyles.account } />
-                <Avatar size={28} icon={ <WechatOutlined /> }  className={ authorStyles.account }  />
+                <Tooltip title="Go to my github">
+                    <a href='https://github.com/Coder-XiaoZhuang' target='_blank'>
+                        <Avatar size={28} icon={ <GithubOutlined /> } className={ authorStyles.account }  />
+                    </a>
+                </Tooltip>
+                <Avatar size={28} icon={ <QqOutlined /> }  className={ authorStyles.account }
+                    onMouseEnter={ () => setState({ isShowQq: true, isShowWechat: false }) }
+                    onMouseLeave={ () => setState({ isShowQq: false, isShowWechat: false }) }
+                ></Avatar>
+                <Avatar size={28} icon={ <WechatOutlined /> }  className={ authorStyles.account }
+                    onMouseEnter={ () => setState({ isShowQq: false, isShowWechat: true }) }
+                    onMouseLeave={ () => setState({ isShowQq: false, isShowWechat: false }) }
+                ></Avatar>
+                {
+                    state.isShowQq ? <div className='authorQq'>  
+                        <div>
+                            <img src={ qqUrl } alt="好好的QQ"/>
+                        </div> 
+                    </div> : null  
+                }
+                {
+                    state.isShowWechat ? <div className='authorWechat'>     
+                    <div>   
+                        <img src={ wechatUrl } alt="好好的微信"/> 
+                    </div> 
+                </div> : null
+                }
             </div> 
         </div>
     );
