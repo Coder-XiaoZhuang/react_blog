07a7688bc8ceb7e778dd334a48e692c3ab0b4037
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2023-03-30 11:03:14 +0800

    feat：新增专栏部分

--- blog/components/Column.js
+++ blog/components/Column.js
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import { Divider } from 'antd';
 import columnStyles from "../static/style/components/Column.module.css";
 
 const Column = () => {
+  let [state, setState] = useState({
+    columnArr: [
+      { columnImg: '', columnTitle: 'vue的学习', columnNum: 9 },
+      { columnImg: '', columnTitle: 'react的学习', columnNum: 9 },
+      { columnImg: '', columnTitle: 'js的学习', columnNum: 9 },
+      { columnImg: '', columnTitle: 'ts的学习', columnNum: 9 },
+      { columnImg: '', columnTitle: 'node的学习', columnNum: 9 },
+    ]
+  });
   return (
     <div className={ `comm-box ${columnStyles.columnDiv}` }>
       <Divider style={{ color: 'pink'  }}>
         TA的专栏
       </Divider>
       <div>
-        目前无数据
+        <div>
+          <ul>
+            <li>
+              <a href="#">
+                <div></div>
+                <img src="" alt="" />
+                <span></span>
+              </a>
+              <div></div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
